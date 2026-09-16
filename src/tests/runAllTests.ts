9d22242936207384ae4337bda35aa48ecfd8df69
elanghaqeem-hash <elang.haqeem@gmail.com>
2026-09-16 09:16:18 +0700

    chore(creative): reconcile latest main validation registry

--- src/tests/runAllTests.ts
+++ src/tests/runAllTests.ts
@@ -4,6 +4,7 @@ import { runToolRouterTests } from './toolRouterTests';
 import { runModelRouterTests } from './modelRouterTests';
 import { runMioLocalProviderTests } from './mioLocalProviderTests';
 import { runTrainingFoundationTests } from './trainingFoundationTests';
+import { runModelPromotionTests } from './modelPromotionTests';
 import { runAiProxyTests } from './aiProxyTests';
 import { runResearchProxyTests } from './researchProxyTests';
 import { runContextContinuityTests } from './contextContinuityTests';
@@ -45,6 +46,7 @@ const runners = [
   runModelRouterTests,
   runMioLocalProviderTests,
   runTrainingFoundationTests,
+  runModelPromotionTests,
   runAiProxyTests,
   runResearchProxyTests,
   runContextContinuityTests,
