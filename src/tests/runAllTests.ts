40e701e9f442d8d28f0c05d90639404a029dabf8
elanghaqeem-hash <elang.haqeem@gmail.com>
2026-09-16 11:39:25 +0700

    TP-0.52 merge signed provenance tests with final promotion gate suite

--- src/tests/runAllTests.ts
+++ src/tests/runAllTests.ts
@@ -12,6 +12,7 @@ import { runTrainingCandidateReviewTests } from './trainingCandidateReviewTests'
 import { runTrainingCandidateLabTests } from './trainingCandidateLabTests';
 import { runAdapterIntegrityTests } from './adapterIntegrityTests';
 import { runAdapterIntegrityReviewGateTests } from './adapterIntegrityReviewGateTests';
+import { runSignedModelArtifactProvenanceTests } from './signedModelArtifactProvenanceTests';
 import { runModelPromotionTests } from './modelPromotionTests';
 import { runModelPromotionServiceTests } from './modelPromotionServiceTests';
 import { runPromotedModelActivationTests } from './promotedModelActivationTests';
@@ -66,6 +67,7 @@ const runners = [
   runTrainingCandidateLabTests,
   runAdapterIntegrityTests,
   runAdapterIntegrityReviewGateTests,
+  runSignedModelArtifactProvenanceTests,
   runModelPromotionTests,
   runModelPromotionServiceTests,
   runPromotedModelActivationTests,
