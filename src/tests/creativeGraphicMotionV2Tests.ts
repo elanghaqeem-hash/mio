9355bdaf86f2f7e87dd5838041710d904a9c001d
elanghaqeem-hash <elang.haqeem@gmail.com>
2026-09-18 11:00:16 +0700

    test(graphic): cover snapped rotation transform

--- src/tests/creativeGraphicMotionV2Tests.ts
+++ src/tests/creativeGraphicMotionV2Tests.ts
@@ -1,4 +1,4 @@
-import { beginGraphicResize, deleteMotionKeyframe, graphicToMotionProject, moveMotionKeyframe, setMotionKeyframeInterpolation, updateGraphicResize, upsertMotionKeyframe } from '../creative/GraphicMotionWorkspace';
+import { beginGraphicResize, deleteMotionKeyframe, graphicToMotionProject, moveMotionKeyframe, setMotionKeyframeInterpolation, updateGraphicResize, beginGraphicRotation, updateGraphicRotation, upsertMotionKeyframe } from '../creative/GraphicMotionWorkspace';
 import type { MioGraphicDocument } from '../types/creative';
 
 interface Result { name:string; passed:boolean; error?:string }
