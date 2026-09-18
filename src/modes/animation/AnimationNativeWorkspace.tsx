ca2ee82350ef9c5e72a1f1e2a07e339ac16646d9
elanghaqeem-hash <elang.haqeem@gmail.com>
2026-09-18 08:20:48 +0700

    fix(animation): import native Action Editor workspace panel

--- src/modes/animation/AnimationNativeWorkspace.tsx
+++ src/modes/animation/AnimationNativeWorkspace.tsx
@@ -5,6 +5,7 @@ import{useCreativeStudioDocument}from'../../creative/useCreativeStudioDocument';
 import{Animation3DViewportBridge}from'./Animation3DViewportBridge';
 import{AnimationDopeSheet}from'./AnimationDopeSheet';
 import{AnimationGraphEditor}from'./AnimationGraphEditor';
+import{AnimationActionEditor}from'./AnimationActionEditor';
 import{AnimationEditorWorkspaceProvider,useAnimationEditorWorkspace}from'./AnimationEditorWorkspaceContext';
 import{selectAnimationBone,setAnimationEditorTime}from'./AnimationEditorState';
 const INITIAL_PROJECT:MioAnimationProject={duration:5,fps:60,currentTime:0,loop:true,tracks:[],rigs:[{id:'rig_vanguard',name:'Vanguard Rig',objectId:'Vanguard_Mech_Hull',bones:[{id:'root',name:'Root',length:1.2,connected:false,ikFk:'FK',pose:{position:[0,0,0],rotation:[0,0,0],scale:[1,1,1]}},{id:'spine',name:'Spine',parentId:'root',length:1,connected:true,ikFk:'FK',pose:{position:[0,1.2,0],rotation:[0,0,0],scale:[1,1,1]}},{id:'arm_l',name:'Arm.L',parentId:'spine',length:.9,connected:false,ikFk:'FK',pose:{position:[-.55,.7,0],rotation:[0,0,.35],scale:[1,1,1]}},{id:'arm_r',name:'Arm.R',parentId:'spine',length:.9,connected:false,ikFk:'FK',pose:{position:[.55,.7,0],rotation:[0,0,-.35],scale:[1,1,1]}}]}]};
